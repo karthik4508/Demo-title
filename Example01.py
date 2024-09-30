334aee5283bec5a085076fab700ea49da6a87835
karthik4508 <oruguntakarthi@gmail.com>
2024-09-30 15:10:29 +0530

    Updated by Karthik_deom_branch_commit2

--- Example01.py
+++ Example01.py
@@ -1,9 +1,19 @@
-def add():
-    a,b=10,5
-    print (a+b)
-    print(a-b)
-    print(a*b)
-    print(a/b)
-add()
+a,b=10,5
 
+def add():
+    print (a+b)
+
+def sub():
+    print(a-b)
+
+def mul():
+    print(a*b)
+
+def div():
+    print(a/b)
+
+add()
+sub()
+mul()
+div()
 
